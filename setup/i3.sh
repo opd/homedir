f4025b7e0a0b0a535446fd0699377f5ec34d9f4e
Andrew <andrewfrommoon@gmail.com>
2019-10-22 10:56:35 +0300

    Add authentication client

--- setup/i3.sh
+++ setup/i3.sh
@@ -1,5 +1,5 @@
 sudo apt install -y i3 dmenu i3status x11-xkb-utils i3blocks
-sudo apt install -y py3status gsimplecal
+sudo apt install -y py3status gsimplecal lxpolkit
 # Screenshot util
 # sudo apt install -y flameshot
 cd from_source
--- setup/yadisk.sh
+++ setup/yadisk.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo "HELLO"
 echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/yandex-disk.list > /dev/null
 wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y yandex-disk
+# НЕ РАБОТАЕТ
+#sudo add-apt-repository ppa:slytomcat/ppa -y
+#sudo apt-get update
+#sudo apt-get install -y yd-tools
+# TODO prompt username
+# yandex-disk token USERNAME
