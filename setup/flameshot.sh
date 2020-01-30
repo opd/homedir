96b126442447a787cec28986e1aa8fcf1f236c20
Andrew <andrewfrommoon@gmail.com>
2020-01-30 22:59:26 +0300

    Fix setup scripts

--- setup/flameshot.sh
+++ setup/flameshot.sh
@@ -1,4 +1,4 @@
-sudo apt install g++ build-essential qt5-default qt5-qmake qttools5-dev-tools libqt5svg5-dev
+sudo apt install -y g++ build-essential qt5-default qt5-qmake qttools5-dev-tools libqt5svg5-dev
 mkdir -p ~/from_source
 cd ~/from_source
 git clone --branch master --single-branch --depth 1 https://github.com/lupoDharkael/flameshot.git
