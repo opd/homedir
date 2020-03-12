772b5dc641fcf080486dafc2f779aee87dc4f5a6
Andrew <andrewfrommoon@gmail.com>
2020-03-12 10:16:40 +0300

    Add gnome screensaver

--- setup/i3.sh
+++ setup/i3.sh
@@ -1,4 +1,4 @@
-sudo apt install -y i3 dmenu i3status x11-xkb-utils i3blocks
+sudo apt install -y i3 dmenu i3status x11-xkb-utils i3blocks gnome-screensaver
 sudo apt install -y py3status gsimplecal lxpolkit
 # Screenshot util
 # sudo apt install -y flameshot
