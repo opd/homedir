8e8afbae3c0e656801789d970a7f9ae1e6b08052
Andrew <andrewfrommoon@gmail.com>
2020-08-29 01:28:44 +0300

    Continue

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -1,4 +1,7 @@
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+# Installation
+# https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions-enterprise-linux-fedora-and-snap-packages
+# https://github.com/nodesource/distributions/blob/master/README.md
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 # https://github.com/nvm-sh/nvm#install--update-script
